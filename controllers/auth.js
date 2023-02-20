8561ce4ce73c9e23b44e9e52f2209c443d2fdcd6
Ognyan Aleksiev <ognyan.aleksiev@foundry4.com>
2023-02-20 15:39:29 +0200

    add packages and user

---
+++ controllers/auth.js
@@ -0,0 +1,9 @@
+const {validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+exports.signup = async(req, res, next) => {
+    
+}
